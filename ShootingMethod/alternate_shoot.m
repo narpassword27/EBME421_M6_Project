489152b78237f177a66ccf7ada48fb1e46d36870
map182 <map182@case.edu>
2019-11-21 10:15:47 -0500

    check this one

--- ShootingMethod/alternate_shoot.m
+++ ShootingMethod/alternate_shoot.m
@@ -2,24 +2,41 @@ function shoot_method
 clc
 clear all
 x=0.5;
-x1=fzero(@solver,x);
+a = 25;
+eps = 0.1;
+c = -2;
+%theta = 0.25;
+
+theta = (c*(1-exp(-a)))/(2*(-c^2 + c - eps));
+
+
+
+
+
+
+
+
+
+x1=fzero(@(x)solver(x,a,eps,c,theta),x);
 end
 
-function F = solver(x)
+function F = solver(x,a,eps,c,theta)
 options=odeset('RelTol', 1e-8, 'AbsTol', [1e-8, 1e-8]);
-[t,u]=ode45(@equation, [0,1], [1,x], options);
+[t,u]=ode45(@(z,u)uz2(z,u,a,eps,c), [0,a], [theta x], options);
 s=length(t);
-F=u(s,1)-exp(1);
+F=u(s,1)-theta;
 figure(1)
-plot(t,u(:,1))
-
+%plot(t,u(:,1))
+plot(t,u(:,1),'-o',t,u(:,2),'-o')
 end
 
-function dy = equation(t,y)
-dy=zeros(2,1);
-dy(1)=y(2);
-dy(2)=t*exp(2*t)+y(1)-t*(y(2)^2);
+function dy = uz2(x,y,a,eps,c)
+    dy=zeros(2,1);
+    dy(1) = y(2);
+    dy(2) =  (y(2)/c) - eps*y(1)/c^2 - (w(x) - w(x-a))/c;
 end
 
-
+function val = w(x)
+    val = exp(-1*abs(x))/2;
+end
 
