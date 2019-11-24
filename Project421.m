ea9211e73d416042ec57aa540ee04c000d5f2062
map182 <map182@case.edu>
2019-11-24 16:32:36 -0500

    Real interop

--- Project421.m
+++ Project421.m
@@ -1,7 +1,7 @@
 % Project EBME 421
 % map182 ngm44 ixy40
 
-runFirstNeuron()
+[I,t] = runFirstNeuron()
 
 
 
--- runFirstNeuron.m
+++ runFirstNeuron.m
@@ -8,20 +8,11 @@ function [I,t] = runFirstNeuron()
 %Run first neuron file
 !nrn\neuron1.hoc
 
+%Allow Neuron to complete simulation before reading
+pause(5);
+
 %Look for neuron1Result.txt, load into memory
-resultFile = load('nrn\neuron1Result.txt')
-
-
-
-
-
-
-
-
-
-
-
-I=0;
-t=0;
+I = load('nrn\neuron1Result.txt').';
+t = 0:0.025:50;
 end
 
